a5916011240b839c45cb731561d4bf7325db7848
Yuri Niyazov <yuri.niyazov@gmail.com>
2015-09-07 20:05:34 -0700

    First commit

---
+++ aws.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+         allowed_account_ids = ["883753636031"]
+         region = "us-east-1"
+}
+
+resource "aws_vpc" "dev" {
+        cidr_block = "10.0.0.0/16"         
+}
+
+resource "aws_security_group" "allow_home_in" {
+  name = "allow_all_in"
+  description = "Allow all inbound traffic from home IP addresses"
+  vpc_id = "${aws_vpc.dev.id}"
+  
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["50.168.114.235/32"]
+  }
+}
